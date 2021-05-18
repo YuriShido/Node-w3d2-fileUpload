cc8d24b04d809eea48a9bf3eaa5e0b1722dfc5d2
YuriShido <sydonce57@gmail.com>
2021-05-18 11:57:15 -0700

    add PDF invoice

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,3 +1,7 @@
+const fs = require('fs')
+const path = require('path')
+const PDFDocument = require('pdfkit')
+const Order = require('../models/Orders')
 const Product = require('../models/Products')
 
 exports.getProducts = (req, res, next) => {
@@ -58,4 +62,88 @@ exports.postCartDeleteProduct = (req,res,next) => {
     req.user.removeFromCart(prodId).then(() => {
         res.redirect('/cart')
     }).catch(err => console.log(err))
+}
+
+exports.postOrder = (req, res, next) => {
+  // console.log('user: ',req.user)
+  req.user
+    .populate('cart.items.productId')
+    .execPopulate()
+    .then(user => {
+      const products = user.cart.items.map(item => {
+        return {
+          product: { ...item.productId._doc },
+          quantity: item.quantity
+        }
+      })
+
+      const order = new Order({
+        products: products,
+        user: {
+          email: req.user.email,
+          userId: req.user //mongoose will only pull out the _id
+        }
+      })
+
+      return order.save()
+    })
+    .then(() => {
+      return req.user.clearCart()
+    })
+    .then(() => {
+      res.redirect('/orders')
+    })
+    .catch((err) => console.log(err))
+}
+
+exports.getOrders = (req, res, next) => {
+  Order.find({ 'user.userId': req.user._id }).then((order) => {
+    res.render('shop/orders', {
+      pageTitle: 'Your Order',
+      path: '/orders',
+      orders: order
+    })
+  }).catch(err => console.log(err))
+}
+
+exports.getInvoice = (req,res, next) => {
+  const orderId = req.params.orderId
+
+  Order.findById(orderId).then((order) => {
+    if(!order){
+      return res.redirect('/')
+    }
+
+    if(order.user.userId.toString() !== req.user._id.toString()) {
+      const error = new Error(err)
+      error.httpStatusCode = 401;
+      return next(error)
+    }
+
+    const invoiceName = 'invoice-' + orderId + '.pdf'
+    const invoivePath = path.join('data', 'invoices', invoiceName)
+
+    const pdfDoc = new PDFDocument()
+    res.setHeader('Content-Type', 'application/pdf')
+    res.setHeader('Content-Disposition', 'inline; filename="' + invoiceName + '"')
+
+    pdfDoc.pipe(fs.createWriteStream(invoivePath))
+    pdfDoc.pipe(res)
+    pdfDoc.fontSize(26).text('Invoice', {
+      underline: true
+    })
+    pdfDoc.text('-----------------------------------')
+    let totalPrice = 0
+    order.products.forEach(prod => {
+      totalPrice += prod.quantity * prod.product.price
+      pdfDoc.fontSize(14).text(
+        prod.product.title + ' - ' + prod.quantity + ' x ' + '$' + prod.product.price
+      )
+    })
+    pdfDoc.text('-----------------------------------')
+    pdfDoc.fontSize(20).text('Total Price: $' + totalPrice)
+
+    pdfDoc.end()
+
+  }).catch(err => console.log(err))
 }
